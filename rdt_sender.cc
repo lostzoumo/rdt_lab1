cbccfb226ac1e9614bd409fd89aae5324104b783
lost <603548406@qq.com>
2016-03-10 22:14:11 -0800

    start

---
+++ rdt_sender.cc
@@ -0,0 +1,114 @@
+/*
+ * FILE: rdt_sender.cc
+ * DESCRIPTION: Reliable data transfer sender.
+ * NOTE: This implementation assumes there is no packet loss, corruption, or 
+ *       reordering.  You will need to enhance it to deal with all these 
+ *       situations.  In this implementation, the packet format is laid out as 
+ *       the following:
+ *       
+ *       |<-  1 byte  ->|<-             the rest            ->|
+ *       | payload size |<-             payload             ->|
+ *
+ *       The first byte of each packet indicates the size of the payload
+ *       (excluding this single-byte header)
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rdt_struct.h"
+#include "rdt_sender.h"
+
+
+/* sender initialization, called once at the very beginning */
+void Sender_Init()
+{
+    fprintf(stdout, "At %.2fs: sender initializing ...\n", GetSimulationTime());
+}
+
+/* sender finalization, called once at the very end.
+   you may find that you don't need it, in which case you can leave it blank.
+   in certain cases, you might want to take this opportunity to release some 
+   memory you allocated in Sender_init(). */
+void Sender_Final()
+{
+    fprintf(stdout, "At %.2fs: sender finalizing ...\n", GetSimulationTime());
+}
+
+char checksum_odd(char *pkt,int num)
+{
+	int checksum=0;
+	for(int i=1;i<num;i+=2){
+		checksum+=pkt[i];
+	}
+	return checksum && 0xFF;
+}
+
+char checksum_even(char *pkt,int num)
+{
+	int checksum=0;
+	for(int i=0;i<num;i+=2){
+		checksum+=pkt[i];
+	}
+	return checksum && 0xFF;
+}
+/* event handler, called when a message is passed from the upper layer at the 
+   sender */
+void Sender_FromUpperLayer(struct message *msg)
+{
+    /* 1-byte header indicating the size of the payload */
+    int header_size = 5;
+
+    /* maximum payload size */
+    int maxpayload_size = RDT_PKTSIZE - header_size;
+
+    /* split the message if it is too big */
+
+    /* reuse the same packet data structure */
+    packet pkt;
+
+    /* the cursor always points to the first unsent byte in the message */
+    int cursor = 0;
+    int seqnum=1;
+    while (msg->size-cursor > maxpayload_size) {
+	/* fill in the packet */
+	pkt.data[0] = maxpayload_size;
+	pkt.data[1] = seqnum;
+	pkt.data[2] = 0;
+	memcpy(pkt.data+header_size, msg->data+cursor, maxpayload_size);
+	pkt.data[3] = checksum_odd(pkt.data+header_size,maxpayload_size);
+	pkt.data[4] = checksum_even(pkt.data+header_size,maxpayload_size);
+	/* send it out through the lower layer */
+	Sender_ToLowerLayer(&pkt);
+
+	/* move the cursor */
+	cursor += maxpayload_size;
+	seqnum++;
+    }
+
+    /* send out the last packet */
+    if (msg->size > cursor) {
+	/* fill in the packet */
+	pkt.data[0] = msg->size-cursor;
+	pkt.data[1] = seqnum;
+	pkr.data[2] = 0;
+	memcpy(pkt.data+header_size, msg->data+cursor, pkt.data[0]);
+	pkt.data[3] = checksum_odd(pkt.data+header_size,maxpayload_size);
+	pkt.data[4] = checksum_even(pkt.data+header_size,maxpayload_size);
+	/* send it out through the lower layer */
+	Sender_ToLowerLayer(&pkt);
+    }
+
+
+/* event handler, called when a packet is passed from the lower layer at the 
+   sender */
+void Sender_FromLowerLayer(struct packet *pkt)
+{
+}
+
+/* event handler, called when the timer expires */
+void Sender_Timeout()
+{
+}
